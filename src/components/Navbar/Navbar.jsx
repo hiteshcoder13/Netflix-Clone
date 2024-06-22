7c37455080230d4d4f9a939aee22c23e7cd2e0d0
Hitesh Kumar <nagpalhitesh521@gmail.com>
2024-06-22 11:45:50 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ import './Navbar.css'
  import { useState } from 'react'
 
  import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from 'react-router-dom'
 
  
 
@@ -70,7 +71,7 @@ import './Navbar.css'
             <img src={profile_img} alt="" srcset="" className='profile' />
             <img src={caret_icon} alt="" srcset=""  />
 <div className="dropdown">
-<Link to={'./'}>Sign Out Of Netflix</Link>
+<Link to={'/'}>Sign Out</Link>
 </div>
          </div>
         </div>
